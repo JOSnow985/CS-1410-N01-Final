cb8ccd0ce0374ef2edaf0a03e3a5831d7c66ab10
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 09:36:42 -0600

    Comment out stubs

--- PolybiusTwo.Tests/UnitTest1.cs
+++ PolybiusTwo.Tests/UnitTest1.cs
@@ -1,15 +1,15 @@
 namespace PolybiusTwo.Tests;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+// public class Tests
+// {
+//     [SetUp]
+//     public void Setup()
+//     {
+//     }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
+//     [Test]
+//     public void Test1()
+//     {
+//         Assert.Pass();
+//     }
+// }
