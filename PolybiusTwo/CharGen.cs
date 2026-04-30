6d79cf36c3a16d5c087bf1bc77d023f65783a935
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 01:48:50 -0600

    CreateNewCharacter() stub

--- PolybiusTwo/CharGen.cs
+++ PolybiusTwo/CharGen.cs
@@ -3,5 +3,14 @@
 public static class CharGen
 {
     // Methods
-
+    public static Character CreateNewCharacter()
+    {
+        // collect name
+        // collect description
+        // collect class choice
+        // collect attribute roll style
+        // roll attributes
+        // return newly created character
+        return null;
+    }
 }
