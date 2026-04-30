d5621a59387da1537257d0eebf29a0018b99040f
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 01:42:59 -0600

    swapped to primary constructor

--- PolybiusTwo/Character.cs
+++ PolybiusTwo/Character.cs
@@ -1,15 +1,16 @@
 ﻿namespace PolybiusTwo;
 
-public class Character
+
+public class Character(string characterName, string description, CharClass charClass, List<(Attribute attribute, int value)> attributes)
 {
     // --- Fields ---
-    private string _name;
-    private string _description;
-    private CharClass _charClass;
+    private string _name = characterName;
+    private string _description = description;
+    private CharClass _charClass = charClass;
+    private List<(Attribute attribute, int value)> _attributes = attributes;
     // private int _maxHealth => _attributes.Constitution;
     private int _maxStamina;
     private int _level;
-    private List<(Attribute attribute, int value)> _attributes;
 
     // --- Properties ---
     public string Name { get => _name; private set => _name = value; }
@@ -19,19 +20,4 @@ public class Character
     // public int MaxHealth { get => _maxHealth; set => _maxHealth = value; }
     public int MaxStamina { get => _maxStamina; set => _maxStamina = value; }
     public int Level { get => _level; set => _level = value; }
-
-
-    // character class
-    // skill list
-    // ability list
-
-    // --- Constructor ---
-    public Character(string characterName)
-    {
-        _name = characterName;
-        // select class
-        // roll attributes
-        // use class and attributes to set up basic stats
-        // 
-    }
 }
