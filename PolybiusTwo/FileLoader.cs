dcf57228193f4a17c23475e122c638edf7363ddb
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 03:46:34 -0600

    LoadFile() method for catching FileNotFoundException

--- PolybiusTwo/FileLoader.cs
+++ PolybiusTwo/FileLoader.cs
@@ -3,13 +3,11 @@
 public static class FileLoader
 {
     // Methods to load files
-    // Loads Core Attribute list from file and returns it
+    // Loads core_attributes.csv and parses it into a list of Attribute objects to return
     public static List<Attribute> LoadCoreAttributes()
     {
         List<Attribute> list = [];
-
-        // Attributes are read from core_attributes.csv
-        string[] lines = File.ReadAllLines("core_attributes.csv");
+        string[] lines = LoadFile("core_attributes.csv");
 
         // Split each line by ',', add the two parts to the list as a new Attribute object
         foreach(string line in lines)
@@ -19,5 +17,23 @@ public static class FileLoader
         }
 
         return list;
+    }
+
+    // Loads the indicated file, returns an empty array if the file's missing
+    public static string[] LoadFile(string filepath)
+    {
+        // Basic catch for a missing file
+        string[] lines;
+        try
+        {
+            // Attributes are read from core_attributes.csv
+            lines = File.ReadAllLines(filepath);
+        }
+        catch (FileNotFoundException)
+        {
+            lines = [];
+        }
+
+        return lines;
     }
 }
