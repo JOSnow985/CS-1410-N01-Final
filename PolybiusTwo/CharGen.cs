ee2b134aa517b6f3f00894c504a3b83c27d6799f
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 01:47:55 -0600

    Character generator class

---
+++ PolybiusTwo/CharGen.cs
@@ -0,0 +1,16 @@
+﻿namespace PolybiusTwo;
+
+public static class CharGen
+{
+    // Methods
+    public static Character CreateNewCharacter()
+    {
+        // collect name
+        // collect description
+        // collect class choice
+        // collect attribute roll style
+        // roll attributes
+        // return newly created character
+        return null;
+    }
+}
