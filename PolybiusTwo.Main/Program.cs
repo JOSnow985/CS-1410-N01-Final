e776752b467c39216697305b32ffa1f9a19e7387
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 09:53:34 -0600

    Main Menu skeleton

--- PolybiusTwo.Main/Program.cs
+++ PolybiusTwo.Main/Program.cs
@@ -1,2 +1,30 @@
 ﻿// Jaden Olvera, Final Project
-Console.WriteLine("placeholder");
+
+while (true)
+{
+    Console.Clear();
+    Menu.ScreenHeader(Menu.MainMenuHeader);
+    switch (Console.ReadKey(true).KeyChar)
+    {
+        case '1':   // Create a Character
+            CharGen.CreateNewCharacter();
+            Menu.CharacterList();
+            break;
+        case '2':   // Character List
+            Menu.CharacterList();
+            break;
+        case '3':   // Class List
+            //
+            break;
+        case '4':   // Attribute List
+            Console.Clear();
+
+            break;
+        case '5':   // Exit
+            Console.Clear();
+            Console.WriteLine("Bye!");
+            return;
+        default:
+            break;
+    }
+}
