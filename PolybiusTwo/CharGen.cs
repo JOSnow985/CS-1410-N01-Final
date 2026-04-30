d81893f674497be59f95d1d23d64947cc9b2cf7f
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 01:54:43 -0600

    Stubs for component methods in CharGen

--- PolybiusTwo/CharGen.cs
+++ PolybiusTwo/CharGen.cs
@@ -13,4 +13,37 @@ public static class CharGen
         // return newly created character
         return null;
     }
+    public static string CollectName()
+    {
+        // ask user for character name
+        // verify name isn't null
+        // return name
+        return null;
+    }
+    public static string CollectDescription()
+    {
+        // ask user if they want to enter a description
+        // if yes, collect description
+        // verify collected description
+        // if no, use base description
+        // return description
+        return null;
+    }
+    public static CharClass ChooseClass()
+    {
+        // present user a list of class options to choose from
+        // allow selecting class to view description
+        // ask to confirm
+        // allow going back to class list screen
+        // return class
+        return null;
+    }
+    public static List<(Attribute, int)> RollAttributes()
+    {
+        // present user a list of attribute roll styles to choose from
+        // roll attributes
+        // show user the generated numbers and let them reroll
+        // return list of attributes
+        return [];
+    }
 }
