0830e5fccf724e432bafeea09ef2b4050a61bed9
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 03:32:42 -0600

    GameCore and FileLoader classes, basic ability csv to load

---
+++ PolybiusTwo/FileLoader.cs
@@ -0,0 +1,23 @@
+﻿namespace PolybiusTwo;
+
+public static class FileLoader
+{
+    // Methods to load files
+    // Loads Core Attribute list from file and returns it
+    public static List<Attribute> LoadCoreAttributes()
+    {
+        List<Attribute> list = [];
+
+        // Attributes are read from core_attributes.csv
+        string[] lines = File.ReadAllLines("core_attributes.csv");
+
+        // Split each line by ',', add the two parts to the list as a new Attribute object
+        foreach(string line in lines)
+        {
+            string[] strings = line.Split(',');
+            list.Add(new(strings[0], strings[1]));
+        }
+
+        return list;
+    }
+}
---
+++ PolybiusTwo/GameCore.cs
@@ -0,0 +1,16 @@
+﻿namespace PolybiusTwo;
+
+public static class GameCore
+{
+    // --- Fields ---
+    // These are lists of core objects we'll reference from other places, loaded from files
+    private static readonly List<Attribute> coreAttributes = FileLoader.LoadCoreAttributes();
+    private static readonly List<CharClass> coreClasses = [];
+
+    // --- Properties ---
+    public static List<Attribute> CoreAttributes => coreAttributes;
+
+    public static List<CharClass> CoreClasses => coreClasses;
+    // --- Constructor ---
+    // --- Methods ---
+}
