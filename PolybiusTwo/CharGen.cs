9726508d53a55d0323d50c1e0f008bfec57371e1
JOSnow985 <jaden.olvera@students.snow.edu>
2026-04-30 06:19:55 -0600

    ChooseClass() implementation, ouch

--- PolybiusTwo/CharGen.cs
+++ PolybiusTwo/CharGen.cs
@@ -8,7 +8,7 @@ public static class CharGen
     {
         string name = CollectName();
         string description = CollectDescription();
-        // collect class choice
+        CharClass charClass = ChooseClass();
         // collect attribute roll style
         // roll attributes
         // return newly created character
@@ -45,11 +45,72 @@ public static class CharGen
     public static CharClass ChooseClass()
     {
         CurrentStep = "Character Class";
-        // present user a list of class options to choose from
-        // allow selecting class to view description
-        // ask to confirm
-        // allow going back to class list screen
-        // return class
+        CharClass? selection = null;
+        while (selection is null)
+        {
+            CharGenHeader();
+            Console.WriteLine("Press a number to see more info on a class, and choose!\n");
+
+            // Print list of class options to choose from, start index at 1 for convenience
+            for(int i = 0; i < GameCore.CoreClasses.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {GameCore.CoreClasses[i].Name}");
+            }
+
+            // Allow the player to select a class from the class list to inspect
+            ConsoleKey keyInput;
+            int? selectedOption = null;
+            do
+            {
+                keyInput = Console.ReadKey(true).Key;
+
+                if (IsNumericKey(keyInput))
+                {
+                    selectedOption = ConvertKeyToNumber(keyInput);
+                }
+            // We continue the loop while any of these conditions is satisfied.
+            } while (selectedOption is null || selectedOption < 1 || selectedOption > GameCore.CoreClasses.Count);
+
+            // Correct the index so it's 0 indexed again and not an int? either
+            int selectedIndex = selectedOption.Value - 1;
+            CharClass selectedClass = GameCore.CoreClasses[selectedIndex];
+
+            // Now that we have a class to inspect, show the player and ask if they want to lock it in or go back
+            CharGenHeader();
+            Console.WriteLine($"Character Class: {selectedClass.Name}\nDescription: {selectedClass.Description}\nHit Die: {selectedClass.HealthDie}");
+            Console.WriteLine($"\nDo you want to be a {GameCore.CoreClasses[selectedIndex].Name}? Y / N");
+            
+            // Only lock their selection in if they press Y here. 
+            keyInput = ConsoleKey.None;
+            while (keyInput != ConsoleKey.Y && keyInput != ConsoleKey.N)
+            {
+                keyInput = Console.ReadKey(true).Key;
+            }
+            if (keyInput == ConsoleKey.Y)
+                selection = GameCore.CoreClasses[selectedIndex];
+        }
+
+        // If we exit the above loop, we've either broken something or we have our class pick, return!
+        return selection;
+    }
+
+    // Checks the passed ConsoleKey to see if it's a number key we can convert to an int
+    private static bool IsNumericKey(ConsoleKey key)
+    {
+        return  key >= ConsoleKey.D0 && key <= ConsoleKey.D9 ||
+                key >= ConsoleKey.NumPad0 && key <= ConsoleKey.NumPad9;
+    }
+
+    // Takes a ConsoleKey and returns an int from it, this can be a switch expression but it's honestly easier to understand this way
+    private static int? ConvertKeyToNumber(ConsoleKey key)
+    {
+        if (key >= ConsoleKey.D0 && key <= ConsoleKey.D9)
+            return key - ConsoleKey.D0;
+
+        // Converts number pad too! (even though I don't have one)
+        if (key >= ConsoleKey.NumPad0 && key <= ConsoleKey.NumPad9)
+            return key - ConsoleKey.NumPad0;
+
         return null;
     }
     public static List<(Attribute, int)> RollAttributes()
