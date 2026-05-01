38058f03d72931dfb506a72b2c68f361eeb2fb5f
JOSnow985 <jaden.olvera@students.snow.edu>
2026-05-01 15:04:27 -0600

    Switched back to a dedicated constructor and added optional level parameter

--- PolybiusTwo/Character.cs
+++ PolybiusTwo/Character.cs
@@ -1,14 +1,14 @@
 ﻿namespace PolybiusTwo;
 
 
-public class Character(string characterName, string description, CharClass charClass, List<(Attribute attribute, int value)> attributes)
+public class Character
 {
     // --- Fields ---
-    private string _name = characterName;
-    private string _description = description;
-    private CharClass _charClass = charClass;
+    private string _name;
+    private string _description;
+    private CharClass _charClass;
     private int _level = 1;
-    private List<(Attribute attribute, int value)> _attributes = attributes;
+    private List<(Attribute attribute, int value)> _attributes;
     // private int _maxHealth => _attributes.Constitution;
     private int _maxStamina;
 
@@ -20,6 +20,21 @@ public class Character(string characterName, string description, CharClass charC
     public List<(Attribute attribute, int value)> Attributes { get => _attributes; set => _attributes = value; }
     // public int MaxHealth { get => _maxHealth; set => _maxHealth = value; }
     public int MaxStamina { get => _maxStamina; set => _maxStamina = value; }
+
+    // --- Constructor ---
+    // Character level is assumed to be 1 if not passed
+    public Character(string name,
+                     string description,
+                     CharClass charClass,
+                     List<(Attribute attribute, int value)> attributes,
+                     int level = 1)
+    {
+        _name = name;
+        _description = description;
+        _charClass = charClass;
+        _level = level;
+        _attributes = attributes;
+    }
 
     // --- Methods ---
     // Save the current character state to a csv file
